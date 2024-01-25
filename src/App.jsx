1240f23530d86150533bf766a4b75949b266cb13
AbiR7171 <abiremtiag@gmail.com>
2024-01-25 18:30:53 +0600

    Animation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  return (
+ <>
+     <section className='main'>
+              <div className='box'></div>
+              <p className='write'>Hi Animation</p>
+     </section>
+ 
+ </>
+  )
+}
+
+export default App
